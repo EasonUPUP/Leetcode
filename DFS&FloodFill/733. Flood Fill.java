967851e841fa82ae37d174ee95b47225d44cdafe
Eason Chen <50070491+EasonUPUP@users.noreply.github.com>
2020-09-28 01:22:16 -0400

    Flood Fill templete

--- DFS&FloodFill/733. Flood Fill.java
+++ DFS&FloodFill/733. Flood Fill.java
@@ -1,4 +1,4 @@
-/* Flood Fill templete : 
+/*  Flood Fill templete : 
 Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
 
 A region is captured by flipping all 'O's into 'X's in that surrounded region.
